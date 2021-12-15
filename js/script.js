a3ee39d1afb87544597e1858dace57d5d3329cdb
Antonella García <alu0101227610@ull.edu.es>
2021-12-15 16:34:52 +0000

    styling, html and scripts done

---
+++ js/script.js
@@ -0,0 +1,28 @@
+(() => {
+	const names = [
+		'Yaakov',
+		'John',
+		'Jen',
+		'Jason',
+		'Paul',
+		'Frank',
+		'Larry',
+		'Paula',
+		'Laura',
+		'Jim',
+    'Antonella'
+	];
+
+	let i, firstLetter;
+
+	for (i = 0; i < names.length; i++) {
+		firstLetter = names[i].charAt(0).toLowerCase();
+
+		if (firstLetter === 'j') {
+			byeSpeaker.speak(names[i]);
+			continue;
+		}
+
+		helloSpeaker.speak(names[i]);
+	}
+})();
